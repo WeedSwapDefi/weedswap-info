173b52b8b10ef809b469a302646ea4e8853df8dc
weedswapdefi <shahzeb8285@gmail.com>
2021-04-22 04:05:58 +0530

    first commit

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="#" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://weedswap.io" rel="noopener noreferrer" target="_blank">
+      WeedSwap
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
